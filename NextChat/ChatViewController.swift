f0442207ca0f63e84c8b8348128593e05e74ea57
Audrey Lim <audim98@icloud.com>
2017-09-08 16:50:47 +0800

    Update ChatViewController

--- NextChat/ChatViewController.swift
+++ NextChat/ChatViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import FirebaseDatabase
+
 
 class ChatViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    @IBAction func buttonSend(_ sender: Any) {
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
